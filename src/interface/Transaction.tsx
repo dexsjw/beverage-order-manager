eeeab6cacb4d27377e6f30c9ec10abadf710d9eb
Soh <dexsjw@gmail.com>
2025-04-11 16:47:30 +0800

    created Transaction interface

---
+++ src/interface/Transaction.tsx
@@ -0,0 +1,10 @@
+import { SessionUser } from "./SessionUser";
+
+export interface Transaction {
+  id: string,
+  timestamp: string,
+  payer: SessionUser,
+  payee: SessionUser,
+  amount: number,
+  isPaid: boolean
+}
