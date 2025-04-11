c47510897bbb1d2cbadf0eb0eb99efa754f74837
dextersjw <dextersjw@gmail.com>
2025-04-11 10:26:52 +0800

    updated Session

--- src/interface/Session.tsx
+++ src/interface/Session.tsx
@@ -1,5 +1,14 @@
+import { Order } from "./Order"
+import { SessionUser } from "./SessionUser"
+
 export interface Session {
     id: string,
     timestamp: string,
-    isActive: boolean
+    isActive: boolean,
+    data: string
+}
+
+export interface SessionData {
+  sessionUsers: SessionUser[],
+  orders: Order[]
 }
