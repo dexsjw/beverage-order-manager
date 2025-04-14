c4190de00b7f03c1f84e93920e074b09fb9c44bd
Soh <dexsjw@gmail.com>
2025-04-14 17:44:36 +0800

    created OrderForm

---
+++ src/components/OrderForm.tsx
@@ -0,0 +1,12 @@
+import { Box, TextField } from "@mui/material";
+
+function OrderForm() {
+
+  return (
+    <Box component="form">
+      <TextField required/>
+    </Box>
+  )
+}
+
+export default OrderForm;
