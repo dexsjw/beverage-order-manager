f94aac4564d9d36f8fdde228689fba249ea815ec
Soh <dexsjw@gmail.com>
2025-04-14 17:43:03 +0800

    added BaseLayout

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,10 +1,36 @@
-import { Box } from "@mui/material";
+import { AppBar, Box, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+import CoffeeIcon from '@mui/icons-material/Coffee';
 
 function BaseLayout() {
   
-  
   return (
-    <Box></Box>
+    <Container>
+      <AppBar>
+        <Toolbar>
+          <Box>
+            <IconButton
+              aria-label="home"
+              edge="end"
+            >
+              <CoffeeIcon />
+            </IconButton>
+          </Box>
+          <Typography variant="h6" component="div" sx={{flexGrow: "1"}}>
+            Beverage Order Manager
+          </Typography>
+          <IconButton
+            aria-label="menu"
+            edge="start"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Outlet />
+    </Container>
   )
 }
 
