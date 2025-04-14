a2d98ac70f21a682f57f4ec5e984d95a95515948
Soh <dexsjw@gmail.com>
2025-04-14 18:13:22 +0800

    created FlexContainerBox

---
+++ src/components/FlexContainerBox.tsx
@@ -0,0 +1,17 @@
+import { Box, Container, styled } from "@mui/material";
+
+export const FlexContainer = styled(Container)({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignContent: "flex-start",
+  alignItems: "flex-start"
+})
+
+export const FlexBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignContent: "flex-start",
+  alignItems: "flex-start"
+})
