05354776bc2210137fc147cb6c26746cc4c82fb6
Soh <dexsjw@gmail.com>
2025-04-14 17:42:26 +0800

    added Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import SessionForm from "../components/SessionForm";
+
+function Home() {
+  return (
+    <Box>
+      <SessionForm />
+    </Box>
+  )
+}
+
+export default Home;
--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-
-function Index() {
-  return (
-    <Box></Box>
-  )
-}
-
-export default Index;
