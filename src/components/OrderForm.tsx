bd25f1ee89cbb76a4a7c5483cc0737845ba58bb9
Soh <dexsjw@gmail.com>
2025-04-16 18:12:05 +0800

    updated OrderForm component

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -1,11 +1,41 @@
-import { Box, TextField } from "@mui/material";
+import { Autocomplete, Stack, TextField } from "@mui/material";
+import { OnePlusKopiMenu } from "../static-data/OnePlusKopi";
 
 function OrderForm() {
 
   return (
-    <Box component="form">
-      <TextField required/>
-    </Box>
+    <Stack spacing={3}>
+      <Autocomplete 
+        options={OnePlusKopiMenu} 
+        renderInput={() => (
+          <TextField 
+            required
+            id="beverage-order" 
+            label="Beverage Order" 
+          />
+        )}
+      />
+      <Autocomplete 
+        options={OnePlusKopiMenu} 
+        renderInput={() => (
+          <TextField 
+            required
+            id="customisations" 
+            label="Customisations" 
+          />
+        )}
+      />
+      <Autocomplete 
+        options={OnePlusKopiMenu} 
+        renderInput={() => (
+          <TextField 
+            required
+            id="quantity" 
+            label="Quantity" 
+          />
+        )}
+      />
+    </Stack>
   )
 }
 
