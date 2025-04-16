dd1aef451a9ee879815660276699c5c8421671da
Soh <dexsjw@gmail.com>
2025-04-16 18:12:54 +0800

    changed and updated from SessionTable to SortableTable

---
+++ src/components/SortableTable.tsx
@@ -0,0 +1,41 @@
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { SortableTableProps } from "../interface/props/SortableTableProps";
+import JoinSessionDialog from "./JoinSessionDialog";
+
+function SortableTable({ tableTitle }: Readonly<SortableTableProps>) {
+  
+  return (
+    <Box>
+      <Typography variant="h6" component="div" sx={{flexGrow: "1"}}>
+        {tableTitle}
+      </Typography>
+      <Paper elevation={12}>
+        <TableContainer>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell>Test Header</TableCell>
+                <TableCell>Test Header</TableCell>
+                <TableCell>Test Header</TableCell>
+                <TableCell>Test Header</TableCell>
+                <TableCell>Test Header</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Test Row</TableCell>
+                <TableCell>Test Row</TableCell>
+                <TableCell>Test Row</TableCell>
+                <TableCell>Test Row</TableCell>
+                <TableCell>Test Row</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <JoinSessionDialog />
+    </Box>
+  )
+}
+
+export default SortableTable;
