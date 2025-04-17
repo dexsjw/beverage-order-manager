2a23a4110f7342361efeffc90f8428a6ad194d9b
Soh <dexsjw@gmail.com>
2025-04-17 16:54:57 +0800

    created BeverageOrderSection

---
+++ src/components/order-form/BeverageOrderSection.tsx
@@ -0,0 +1,32 @@
+import { Autocomplete, Stack, TextField, Typography } from "@mui/material";
+import { Beverage } from "../../interface/Beverage";
+import { OnePlusKopiMenu } from "../../static-data/OnePlusKopi";
+
+function BeverageOrderSection() {
+  return (
+    <Stack spacing={1}>
+      <Typography 
+        variant="body1" 
+        align="left" 
+        component="div"
+      >
+        Beverage Order
+      </Typography>
+      <Autocomplete 
+        id="beverage-order" 
+        options={OnePlusKopiMenu} 
+        getOptionLabel={(option: Beverage) => option.name}
+        renderInput={(params) => (
+          <TextField 
+            {...params}
+            required
+            variant="filled"
+            placeholder="Kopi" 
+          />
+        )}
+      />
+    </Stack>
+  )
+}
+
+export default BeverageOrderSection;
