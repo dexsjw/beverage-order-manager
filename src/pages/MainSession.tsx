012dacd8229c1b25582ba68a38eb24ebdc19c31c
Soh <dexsjw@gmail.com>
2025-05-16 18:55:43 +0800

    updated MainSession with logic

--- src/pages/MainSession.tsx
+++ src/pages/MainSession.tsx
@@ -1,7 +1,25 @@
 import { Box, Tab, Tabs } from "@mui/material";
 import MainSessionOrder from "../components/MainSessionOrder";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSessionContext } from "../context/SessionContext";
+import { Order } from "../type-interface/Order";
 
 function MainSession() {
+  const { sessions } = useSessionContext();
+  const { sessionId } = useParams();
+  const navigate = useNavigate();
+
+  let orders: Order[] = [];
+  let timestamp: string = "";
+  const selectedSession = sessions.find(session => session.id === sessionId);
+
+  if (selectedSession === undefined) {
+    console.error(`Unable to find Session with id: ${sessionId}`);
+    navigate("/");
+  } else {
+    orders = selectedSession.data.orders;
+    timestamp = selectedSession.timestamp;
+  }
 
 
   return (
@@ -14,7 +32,10 @@ function MainSession() {
         <Tab label="One Plus Kopi" />
         <Tab label="Custom" />
       </Tabs>
-      <MainSessionOrder />
+      <MainSessionOrder 
+        orders={orders}
+        timestamp={timestamp}
+      />
     </Box>
   )
 }
