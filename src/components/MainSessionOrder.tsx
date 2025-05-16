6f486d7a6d11ad40144c6d599119c02c46ad3ea9
Soh <dexsjw@gmail.com>
2025-05-16 18:56:15 +0800

    updated MainSessionOrder with required table data

--- src/components/MainSessionOrder.tsx
+++ src/components/MainSessionOrder.tsx
@@ -1,12 +1,28 @@
 import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from "@mui/material";
+import { OrderTableData } from "../type-interface/Order";
+import { MainSessionOrderProps } from "../type-interface/props/MainSessionOrderProps";
+import { TableHeader } from "../type-interface/props/SortableTableProps";
 import OrderForm from "./order-form/OrderForm";
 import SortableTable from "./SortableTable";
 
-function MainSessionOrder() {
-  const tableHeaders = [
-    { id: 1, name: "Name" },
-    { id: 2, name: "Owner" },
-    { id: 3, name: "Status" }
+function MainSessionOrder({ orders, timestamp }: Readonly<MainSessionOrderProps>) {
+  const orderTableDataArray: OrderTableData[] = orders.map(order => ({
+    id: order.id,
+    sessionUser: order.sessionUser.name,
+    takeAway: order.customisations.isTakeAway ? "Take Away" : "Dine In",
+    beverage: order.beverage.name,
+    customisations: `${order.customisations.thicknessLevel}, ${order.customisations.sweetnessLevel}, ${order.customisations.others}`,
+    quantity: order.quantity,
+    price: order.beverage.price * order.quantity
+  }))
+
+  const orderTableHeaders: TableHeader<OrderTableData>[] = [
+    { id: "sessionUser", name: "Name" },
+    { id: "takeAway", name: "Take Away?" },
+    { id: "beverage", name: "Beverage" },
+    { id: "customisations", name: "Customisations" },
+    { id: "quantity", name: "Quantity" },
+    { id: "price", name: "Price" }
   ];
 
   return (
@@ -36,10 +52,10 @@ function MainSessionOrder() {
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <SortableTable 
-            tableTitle="Date: " 
-            tableHeaders={tableHeaders}
-            tableData={[]}
+          <SortableTable<OrderTableData>
+            tableTitle={"Date: " + timestamp} 
+            tableHeaders={orderTableHeaders}
+            tableData={orderTableDataArray}
           />
         </AccordionDetails>
       </Accordion>
@@ -54,10 +70,10 @@ function MainSessionOrder() {
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <SortableTable 
-            tableTitle="Date: " 
-            tableHeaders={tableHeaders}
-            tableData={[]}
+          <SortableTable<OrderTableData>
+            tableTitle={"Date: " + timestamp} 
+            tableHeaders={orderTableHeaders}
+            tableData={orderTableDataArray}
           />
         </AccordionDetails>
       </Accordion>
