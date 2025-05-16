9d7a7ea72d57df06f7411ec8032731b0ff653088
Soh <dexsjw@gmail.com>
2025-05-16 16:25:54 +0800

    used SessionTableData instead of Session

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,25 +3,31 @@ import SessionForm from "../components/SessionForm";
 import SessionUserForm from "../components/SessionUserForm";
 import SortableTable from "../components/SortableTable";
 import { useSessionContext } from "../context/SessionContext";
-import { Session } from "../type-interface/Session";
+import { Session, SessionTableData } from "../type-interface/Session";
 import { TableHeader } from "../type-interface/props/SortableTableProps";
 
 function Home() {
-  const tableHeaders: TableHeader<Session>[] = [
+  const tableHeaders: TableHeader<SessionTableData>[] = [
     { id: "name", name: "Name" },
     { id: "owner", name: "Owner"},
     { id: "isActive", name: "Status" }
   ];
   const { sessions } = useSessionContext();
+  const sessionTableDataArray: SessionTableData[] = sessions.map(session => ({
+    id: session.id,
+    name: session.name,
+    owner: session.owner.name,
+    isActive: session.isActive ? "Active" : "Closed"
+  }))
 
   return (
     <Stack spacing={5}>
       <SessionUserForm />
       <SessionForm />
-      <SortableTable<Session> 
+      <SortableTable<SessionTableData> 
         tableTitle="Join A Session: " 
         tableHeaders={tableHeaders}
-        tableData={sessions}
+        tableData={sessionTableDataArray}
       />
     </Stack>
   )
