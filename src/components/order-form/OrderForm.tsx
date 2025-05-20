8f1c4ce68619a9c8acd90fc7f1d7768c8db25d86
Soh <dexsjw@gmail.com>
2025-05-20 16:57:04 +0800

    updated to use dynamic beverage menu according to selected tab

--- src/components/order-form/OrderForm.tsx
+++ src/components/order-form/OrderForm.tsx
@@ -1,13 +1,19 @@
 import { Stack } from "@mui/material";
+import { AvailableBrands, AvailableBrandsMenu } from "../../static-data/AvailableBrandsMenu";
+import { Beverage } from "../../type-interface/Beverage";
+import { OrderFormProps } from "../../type-interface/props/OrderFormProps";
 import BeverageOrderSection from "./BeverageOrderSection";
 import CustomisationsSection from "./CustomisationsSection";
 import QuantitySection from "./QuantitySection";
 
-function OrderForm() {
+function OrderForm({ selectedBrandIndex }: Readonly<OrderFormProps>) {
+  const brandBeverageMenu: Beverage[] = AvailableBrandsMenu[AvailableBrands[selectedBrandIndex]];
 
   return (
     <Stack spacing={2}>
-      <BeverageOrderSection />
+      <BeverageOrderSection 
+        beverageMenu={brandBeverageMenu}
+      />
       <CustomisationsSection />
       <QuantitySection />
     </Stack>
