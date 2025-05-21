df26e70d0aac83d91594ade5e5cc22f87ac28ad6
Soh <dexsjw@gmail.com>
2025-05-21 16:46:58 +0800

    updated CustomisationsSection to use props

--- src/components/order-form/OrderForm.tsx
+++ src/components/order-form/OrderForm.tsx
@@ -1,20 +1,25 @@
 import { Stack } from "@mui/material";
-import { AvailableBrands, AvailableBrandsMenu } from "../../static-data/AvailableBrandsMenu";
+import { AvailableBrands, AvailableBrandsCustomisations, AvailableBrandsMenu } from "../../static-data/AvailableBrandsData";
 import { Beverage } from "../../type-interface/Beverage";
 import { OrderFormProps } from "../../type-interface/props/OrderFormProps";
 import BeverageOrderSection from "./BeverageOrderSection";
 import CustomisationsSection from "./CustomisationsSection";
 import QuantitySection from "./QuantitySection";
+import { OnePlusKopiCustomisationsOptions } from "../../static-data/OnePlusKopi";
+import { CustomisationsOption } from "../../type-interface/Customisations";
 
 function OrderForm({ selectedBrandIndex }: Readonly<OrderFormProps>) {
   const brandBeverageMenu: Beverage[] = AvailableBrandsMenu[AvailableBrands[selectedBrandIndex]];
+  const brandCustomisationsOptions: CustomisationsOption[] = AvailableBrandsCustomisations[AvailableBrands[selectedBrandIndex]];
 
   return (
     <Stack spacing={2}>
       <BeverageOrderSection 
         beverageMenu={brandBeverageMenu}
       />
-      <CustomisationsSection />
+      <CustomisationsSection 
+        customisationsOptions={brandCustomisationsOptions}
+      />
       <QuantitySection />
     </Stack>
   )
