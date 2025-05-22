ed389b51ce3c45108b01272f2ad0a62161bea03e
Soh <dexsjw@gmail.com>
2025-05-22 17:48:08 +0800

    updated AvailableBrandsData with new values and type

--- src/static-data/AvailableBrandsData.ts
+++ src/static-data/AvailableBrandsData.ts
@@ -2,16 +2,14 @@ import { Beverage } from "../type-interface/Beverage";
 import { CustomisationsOption } from "../type-interface/Customisations";
 import { BRAND_ONE_PLUS_KOPI, OnePlusKopiCustomisationsOptions, OnePlusKopiMenu } from "./OnePlusKopi";
 
-export const AvailableBrands: string[] = [
-  BRAND_ONE_PLUS_KOPI,
-  "custom"
-]
-
-export const AvailableBrandsMenu: {[key: string]: Beverage[]} = {
-  [BRAND_ONE_PLUS_KOPI]: OnePlusKopiMenu,
-  custom: [{ id: 101, category: "Traditional Coffee", name: "Custom Coffee", price: 1.2 }]
+type BrandData = {
+  menu: Beverage[],
+  customisationsOptions: CustomisationsOption[]
 }
 
-export const AvailableBrandsCustomisations: {[key: string]: CustomisationsOption[]} = {
-  [BRAND_ONE_PLUS_KOPI]: OnePlusKopiCustomisationsOptions
+export const AvailableBrandsData: {[key: string]: BrandData} = {
+  [BRAND_ONE_PLUS_KOPI]: { menu: OnePlusKopiMenu, customisationsOptions: OnePlusKopiCustomisationsOptions },
+  custom: {menu: [{ id: 101, category: "Traditional Coffee", name: "Custom Coffee", price: 1.2 }], customisationsOptions: []}
 }
+
+export const AvailableBrands = Object.keys(AvailableBrandsData);
