b496b868c5865e9392f45cf1f6bc3f268d2d127d
Soh <dexsjw@gmail.com>
2025-05-22 17:48:46 +0800

    updated imports for AvailableBrandsData

--- src/components/order-form/OrderForm.tsx
+++ src/components/order-form/OrderForm.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "@mui/material";
-import { AvailableBrands, AvailableBrandsCustomisations, AvailableBrandsMenu } from "../../static-data/AvailableBrandsData";
+import { AvailableBrands, AvailableBrandsData } from "../../static-data/AvailableBrandsData";
 import { Beverage } from "../../type-interface/Beverage";
 import { CustomisationsOption } from "../../type-interface/Customisations";
 import { OrderFormProps } from "../../type-interface/props/OrderFormProps";
@@ -8,8 +8,8 @@ import CustomisationsSection from "./CustomisationsSection";
 import QuantitySection from "./QuantitySection";
 
 function OrderForm({ selectedBrandIndex }: Readonly<OrderFormProps>) {
-  const brandBeverageMenu: Beverage[] = AvailableBrandsMenu[AvailableBrands[selectedBrandIndex]];
-  const brandCustomisationsOptions: CustomisationsOption[] = AvailableBrandsCustomisations[AvailableBrands[selectedBrandIndex]];
+  const brandBeverageMenu: Beverage[] = AvailableBrandsData[AvailableBrands[selectedBrandIndex]].menu;
+  const brandCustomisationsOptions: CustomisationsOption[] = AvailableBrandsData[AvailableBrands[selectedBrandIndex]].customisationsOptions;
 
   return (
     <Stack spacing={2}>
