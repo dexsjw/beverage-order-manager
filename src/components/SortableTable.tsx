8b5caa26aa6d923ff11443316e1315e0cdd8cf6b
dextersjw <dextersjw@gmail.com>
2025-05-25 15:25:18 +0800

    updated selectedRowId to be inherited prop instead

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -1,20 +1,18 @@
 import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import { Key, ReactNode, useState } from "react";
+import { Key, ReactNode } from "react";
 import { SortableTableProps } from "../type-interface/props/SortableTableProps";
 
 function SortableTable<T extends { id: Key, [key: string | number]: ReactNode }>({
   tableTitle, 
   tableHeaders,
   tableData,
+  selectedRowId,
   onRowSelect
   // TODO: include row select function to run when row is selected e.g. open dialog, edit field
 }: Readonly<SortableTableProps<T>>) {
   
-  const [selectedRowId, setSelectedRowId] = useState<Key>("");
-
   const handleRowSelect = (rowId: Key) => {
     onRowSelect(rowId);
-    setSelectedRowId(prevRowId => prevRowId === rowId ? "" : rowId);
   }
   
   return (
