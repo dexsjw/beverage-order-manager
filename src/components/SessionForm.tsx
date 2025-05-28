1e2452b1ee8862500f7a7fb0567f5654785ea7b1
Soh <dexsjw@gmail.com>
2025-05-28 18:35:08 +0800

    updated with logic for controlled input and passwords match validation

--- src/components/SessionForm.tsx
+++ src/components/SessionForm.tsx
@@ -1,27 +1,83 @@
-import { Button, Stack, TextField } from "@mui/material";
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import { SessionFormProps } from "../type-interface/props/SessionFormProps";
+import { Session } from "../type-interface/Session";
+import { ChangeEvent, useState } from "react";
 
-function SessionForm() {
+const newSession: Session = {
+  id: crypto.randomUUID(),
+  name: "",
+  password: "",
+  owner: { id: "", name: "" },
+  timestamp: new Date(Date.now()).toLocaleDateString("en-GB"),
+  isActive: true,
+  data: {
+    orders: [],
+    transactions: []
+  }
+}
 
+function SessionForm({ sessionUser }: SessionFormProps) {
+  const [session, setSession] = useState(newSession);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isPasswordsMatch, setIsPasswordsMatch] = useState<boolean>(true);
+
+  const handleSessionFieldsChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    if (event.target.name === "password") {
+      setIsPasswordsMatch(event.target.value === confirmPassword);
+    }
+    // TODO: Not sure if shallow copy works here
+    setSession(prevSession => {
+      return {
+        ...prevSession,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  const handleConfirmPasswordChange = (confirmPassword: string) => {
+    setConfirmPassword(confirmPassword);
+    setIsPasswordsMatch(session.password === confirmPassword);
+    console.log(session);
+  }
 
   return (
     <Stack spacing={2}>
       <TextField 
         required 
-        id="session-name" 
-        label="Session Name" 
+        id="session-name"
+        name="name"
+        label="Session Name"
+        value={session.name}
+        onChange={(event) => handleSessionFieldsChange(event)}
       />
       <TextField 
         required 
         type="password"
-        id="password" 
-        label="Password" 
+        id="password"
+        name="password"
+        label="Password"
+        value={session.password}
+        onChange={(event) => handleSessionFieldsChange(event)}
       />
       <TextField 
         required 
         type="password"
-        id="confirm-password" 
-        label="Confirm Password" 
+        id="confirm-password"
+        name="confirm-password"
+        label="Confirm Password"
+        value={confirmPassword}
+        onChange={(event) => handleConfirmPasswordChange(event.target.value)}
       />
+      {!isPasswordsMatch && 
+        <Typography 
+          variant="body1" 
+          component="div"
+          align="left"
+          color="error"
+        >
+          Passwords do not match!
+        </Typography>
+      }
       <Button
         variant="contained"
       >
