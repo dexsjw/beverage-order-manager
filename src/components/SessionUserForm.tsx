a4e7d07e5f5f10d0acc86ee982fdcbc1be942c28
Soh <dexsjw@gmail.com>
2025-05-28 15:01:58 +0800

    removed commented code blocks

--- src/components/SessionUserForm.tsx
+++ src/components/SessionUserForm.tsx
@@ -1,54 +1,11 @@
 import EditIcon from '@mui/icons-material/Edit';
 import { IconButton, Stack, TextField } from "@mui/material";
 import { useState } from "react";
-import { SessionUser } from "../type-interface/SessionUser";
-import { FlexBoxRowGap } from "./styled/FlexBox";
 import { SessionUserFormProps } from '../type-interface/props/SessionUserFormProps';
-
-// const BOM_SESSION_USER_KEY = "bomSessionUserKey";
-// const existingSessionUser = localStorage.getItem(BOM_SESSION_USER_KEY);
-// const newSessionUser: SessionUser = { id: crypto.randomUUID(), name: "" };
-// let currentSessionUser: SessionUser = { id: "", name: "" };
-
-// try {
-//   currentSessionUser = existingSessionUser
-//     ? JSON.parse(existingSessionUser)
-//     : newSessionUser;
-//   if (!currentSessionUser.id || currentSessionUser.id.trim() === "") {
-//     console.warn("Removing key...");
-//     localStorage.removeItem(BOM_SESSION_USER_KEY);
-//     currentSessionUser = newSessionUser;
-//   }
-// } catch (error) {
-//   console.error(error);
-//   localStorage.removeItem(BOM_SESSION_USER_KEY);
-// }
+import { FlexBoxRowGap } from "./styled/FlexBox";
 
 function SessionUserForm({ sessionUser, handleSessionUserChange }: Readonly<SessionUserFormProps>) {
   const [isIdFieldDisabled, setIsIdFieldDisabled] = useState(true);
-  // const [sessionUser, setSessionUser] = useState(currentSessionUser);
-
-  // const handleSessionUserIdChange = (sessionUserId: string) => {
-  //   setSessionUser(prevSessionUser => {
-  //     const newSessionUser: SessionUser = {
-  //       ...prevSessionUser,
-  //       id: sessionUserId
-  //     };
-  //     localStorage.setItem(BOM_SESSION_USER_KEY, JSON.stringify(newSessionUser));
-  //     return newSessionUser;
-  //   });
-  // }
-
-  // const handleSessionUserNameChange = (sessionUserName: string) => {
-  //   setSessionUser(prevSessionUser => {
-  //     const newSessionUser: SessionUser = {
-  //       ...prevSessionUser,
-  //       name: sessionUserName
-  //     };
-  //     localStorage.setItem(BOM_SESSION_USER_KEY, JSON.stringify(newSessionUser));
-  //     return newSessionUser;
-  //   });
-  // }
 
   return (
     <Stack spacing={2}>
