704c83bf56c9e0a613d917dbe33077a5520ab698
dextersjw <dextersjw@gmail.com>
2025-06-14 23:13:29 +0800

    updated CustomisationsOption to use discriminated unions

--- src/type-interface/Customisations.ts
+++ src/type-interface/Customisations.ts
@@ -5,13 +5,33 @@ export type Customisations = {
   others: string | null
 }
 
-export type CustomisationsOption = {
-  id: keyof Customisations,
+export type CustomisationsOption = 
+{
+  id: keyof Customisations
   label: string,
   name?: string,
   placeholder?: string,
-  // if dropdown for options is required, any of the "*Options" field below need to be provided
-  stringOptions?: string[],
-  numberOptions?: number[],
-  booleanOptions?: boolean[]
-}
+} & (
+  {
+    type: "boolean",
+    options: boolean[]
+  } |
+  {
+    type: "string",
+    options: string[]
+  } |
+  {
+    type: "number",
+    options: number[]
+  }
+)
+// export type CustomisationsOption = {
+//   id: keyof Customisations,
+//   label: string,
+//   name?: string,
+//   placeholder?: string,
+//   // if dropdown for options is required, any of the "*Options" field below need to be provided
+//   stringOptions?: string[],
+//   numberOptions?: number[],
+//   booleanOptions?: boolean[]
+// }
