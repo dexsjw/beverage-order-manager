6897d9f34fffe0322645936905594c3c32fe466a
Soh <dexsjw@gmail.com>
2025-06-20 18:46:40 +0800

    changed CustomisationsOption field from "id" to "key"

--- src/components/order-form/CustomisationsSection.tsx
+++ src/components/order-form/CustomisationsSection.tsx
@@ -25,19 +25,19 @@ function CustomisationsSection({ customisationsOptions, handleCustomisationsChan
     let errMsg = "";
     switch (customisationsOption.type) {
       case "boolean": {
-        if (isCustomisationsKeyofType<boolean>(initialCustomisations, customisationsOption.id, customisationsOption.type)) {
-          initialCustomisations[customisationsOption.id] = customisationsOption.options[0];
+        if (isCustomisationsKeyofType<boolean>(initialCustomisations, customisationsOption.key, customisationsOption.type)) {
+          initialCustomisations[customisationsOption.key] = customisationsOption.options[0];
         } else {
-          errMsg = `${customisationsOption.id} field has error. Check if customisations options provided correctly.`;
+          errMsg = `${customisationsOption.key} field has error. Check if customisations options provided correctly.`;
         }
         break;
       }
       
       case "string": {
-        if (isCustomisationsKeyofType<string>(initialCustomisations, customisationsOption.id, customisationsOption.type)) {
-          initialCustomisations[customisationsOption.id] = customisationsOption.options[0];
+        if (isCustomisationsKeyofType<string>(initialCustomisations, customisationsOption.key, customisationsOption.type)) {
+          initialCustomisations[customisationsOption.key] = customisationsOption.options[0];
         } else {
-          errMsg = `${customisationsOption.id} field has error. Check if customisations options provided correctly.`;
+          errMsg = `${customisationsOption.key} field has error. Check if customisations options provided correctly.`;
         }
         break;
       }
@@ -66,26 +66,26 @@ function CustomisationsSection({ customisationsOptions, handleCustomisationsChan
     return isAnyNullValue;
   }
 
-  const handleCustomisationsValuesChange = (customisationId: keyof Customisations, customisationValue: string | boolean | number | null) => {
+  const handleCustomisationsValuesChange = (customisationKey: keyof Customisations, customisationValue: string | boolean | number | null) => {
     const newCustomisations: Customisations = {
       ...customisations,
-      [customisationId]: customisationValue
+      [customisationKey]: customisationValue
     };
     setCustomisations(newCustomisations);
 
-    if (customisationId !== CUSTOMISATIONS_OTHERS_FIELD && customisationValue === null) {
-      setIsRequiredCustomisationsNull(true);
+    if (customisationKey !== CUSTOMISATIONS_OTHERS_FIELD && customisationValue === null) {
       setCustomisationsNullFields(prevFields => {
-        const customisationsOption = customisationsOptions.find(customisation => customisation.id === customisationId);
+        const customisationsOption = customisationsOptions.find(customisation => customisation.key === customisationKey);
         return (customisationsOption && !customisationsNullFields.includes(customisationsOption.label)) 
         ? [ ...prevFields, customisationsOption.label ]
         : prevFields;
       });
-      handleCustomisationsChange(emptyCustomisations);
 
+      setIsRequiredCustomisationsNull(true);
+      handleCustomisationsChange(createInvalidCustomisations(newCustomisations, customisationKey));
     } else {
       setCustomisationsNullFields(prevFields => {
-        const customisationsOption = customisationsOptions.find(customisation => customisation.id === customisationId);
+        const customisationsOption = customisationsOptions.find(customisation => customisation.key === customisationKey);
         return customisationsOption 
         ? customisationsNullFields.filter(label => label !== customisationsOption.label)
         : prevFields;
@@ -93,7 +93,7 @@ function CustomisationsSection({ customisationsOptions, handleCustomisationsChan
 
       if (isRequiredCustomisationsValuesNull(newCustomisations)) {
         setIsRequiredCustomisationsNull(true);
-        handleCustomisationsChange(emptyCustomisations);
+        handleCustomisationsChange(createInvalidCustomisations(newCustomisations, customisationKey));
       } else {
         setIsRequiredCustomisationsNull(false);
         handleCustomisationsChange(newCustomisations);
@@ -112,7 +112,7 @@ function CustomisationsSection({ customisationsOptions, handleCustomisationsChan
       </Typography>
       {customisationsOptions.map(customisationsOption => (
         <Stack 
-          key={customisationsOption.id}
+          key={customisationsOption.key}
           spacing={1}
         >
           {customisationsOption.name && 
@@ -126,11 +126,11 @@ function CustomisationsSection({ customisationsOptions, handleCustomisationsChan
           }
           {customisationsOption.type === "boolean" && 
             <Autocomplete 
-              id={customisationsOption.id}
+              id={customisationsOption.key}
               options={customisationsOption.options} 
               getOptionLabel={(option: boolean) => option ? "Yes" : "No"} 
-              value={customisations[customisationsOption.id] as boolean}
-              onChange={(event, newValue) => handleCustomisationsValuesChange(customisationsOption.id, newValue)}
+              value={customisations[customisationsOption.key] as boolean}
+              onChange={(event, newValue) => handleCustomisationsValuesChange(customisationsOption.key, newValue)}
               renderInput={(params) => (
                 <TextField 
                   {...params}
@@ -144,11 +144,11 @@ function CustomisationsSection({ customisationsOptions, handleCustomisationsChan
           }
           {customisationsOption.type === "string" &&
             <Autocomplete 
-              id={customisationsOption.id}
+              id={customisationsOption.key}
               options={customisationsOption.options} 
               getOptionLabel={(option: string) => option} 
-              value={customisations[customisationsOption.id] as string}
-              onChange={(event, newValue) => handleCustomisationsValuesChange(customisationsOption.id, newValue)}
+              value={customisations[customisationsOption.key] as string}
+              onChange={(event, newValue) => handleCustomisationsValuesChange(customisationsOption.key, newValue)}
               renderInput={(params) => (
                 <TextField 
                   {...params}
