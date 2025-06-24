adecf81f591f0792d07bd318fd4532361a7d2785
Soh <dexsjw@gmail.com>
2025-06-24 18:50:01 +0800

    started logic for handling quantity change

--- src/components/order-form/QuantitySection.tsx
+++ src/components/order-form/QuantitySection.tsx
@@ -2,16 +2,22 @@ import { Stack, TextField, Typography } from "@mui/material";
 import { QuantitySectionProps } from "../../type-interface/props/QuantitySectionProps";
 import { useState } from "react";
 
+// TODO: fix regex
+const numberOnlyRegex = /\d/;
 const initialQuantity = 1;
 
 function QuantitySection({ handleQuantityChange }: Readonly<QuantitySectionProps>) {
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(initialQuantity);
 
-  const handleQuantityValueChange = (quantityValue: string) => {
-    if (false) {
-
+  const handleQuantityValueChange = (quantityStrValue: string) => {
+    console.log(quantityStrValue);
+    console.log(numberOnlyRegex.test(quantityStrValue.trim()));
+    if (numberOnlyRegex.test(quantityStrValue.trim())) {
+      // TODO: to set as "0" if no value is present
+      const quantityValue = quantityStrValue === "" ? 0 : parseInt(quantityStrValue);
+      setQuantity(quantityValue);
+      handleQuantityChange(quantityValue);
     }
-    parseInt(quantityValue);
   }
 
   return (
@@ -25,11 +31,15 @@ function QuantitySection({ handleQuantityChange }: Readonly<QuantitySectionProps
     </Typography>
       <TextField 
         required
-        // type="number"
         variant="filled"
         id="quantity"
         name="quantity"
         label="Quantity"
+        slotProps={{
+          htmlInput: {
+            inputMode: "numeric"
+          }
+        }}
         value={quantity}
         onChange={(event) => handleQuantityValueChange(event.target.value)}
       />
