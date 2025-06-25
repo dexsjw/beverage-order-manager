98f5339664fd09b6074a19dc8cc34813da331343
Soh <dexsjw@gmail.com>
2025-06-25 15:39:38 +0800

    implemented logic for handling quantity change

--- src/components/order-form/QuantitySection.tsx
+++ src/components/order-form/QuantitySection.tsx
@@ -2,21 +2,19 @@ import { Stack, TextField, Typography } from "@mui/material";
 import { QuantitySectionProps } from "../../type-interface/props/QuantitySectionProps";
 import { useState } from "react";
 
-// TODO: fix regex
-const numberOnlyRegex = /\d/;
+const numbersOnlyRegex = /^\d{0,3}$/;
 const initialQuantity = 1;
 
 function QuantitySection({ handleQuantityChange }: Readonly<QuantitySectionProps>) {
   const [quantity, setQuantity] = useState(initialQuantity);
 
   const handleQuantityValueChange = (quantityStrValue: string) => {
-    console.log(quantityStrValue);
-    console.log(numberOnlyRegex.test(quantityStrValue.trim()));
-    if (numberOnlyRegex.test(quantityStrValue.trim())) {
-      // TODO: to set as "0" if no value is present
+    if (numbersOnlyRegex.test(quantityStrValue.trim())) {
       const quantityValue = quantityStrValue === "" ? 0 : parseInt(quantityStrValue);
       setQuantity(quantityValue);
-      handleQuantityChange(quantityValue);
+      if (quantityValue >= 1) {
+        handleQuantityChange(quantityValue);
+      }
     }
   }
 
@@ -29,20 +27,30 @@ function QuantitySection({ handleQuantityChange }: Readonly<QuantitySectionProps
     >
       Quantity
     </Typography>
-      <TextField 
-        required
-        variant="filled"
-        id="quantity"
-        name="quantity"
-        label="Quantity"
-        slotProps={{
-          htmlInput: {
-            inputMode: "numeric"
-          }
-        }}
-        value={quantity}
-        onChange={(event) => handleQuantityValueChange(event.target.value)}
-      />
+    <TextField 
+      required
+      variant="filled"
+      id="quantity"
+      name="quantity"
+      label="Quantity"
+      slotProps={{
+        htmlInput: {
+          inputMode: "numeric"
+        }
+      }}
+      value={quantity}
+      onChange={(event) => handleQuantityValueChange(event.target.value)}
+    />
+    {quantity < 1 && 
+      <Typography 
+        variant="body1" 
+        component="div"
+        align="left"
+        color="error"
+      >
+        Minimum quantity is 1!
+      </Typography>
+    }
   </Stack>
   )
 }
