c2603048ac6e2e21ece06c9ee8d384a64b8b764f
Soh <dexsjw@gmail.com>
2025-07-07 18:42:01 +0800

    added mockPutSessionDetails() and mockPostOrder()

--- src/api-service/mock-service.tsx
+++ src/api-service/mock-service.tsx
@@ -1,6 +1,7 @@
 import { AxiosResponse } from "axios";
 import { Session } from "../type-interface/Session";
 import mockApi from "./mock-api";
+import { Order } from "../type-interface/Order";
 
 const URL_SESSIONS = "/sessions/";
 const URL_SESSION_DETAILS = "/session-details/";
@@ -43,6 +44,27 @@ export const mockPostSessionDetails = async (session: Session) => {
     const response: AxiosResponse<Session> = await mockApi.post(URL_SESSION_DETAILS, session);
     const sessionDetails: Session = response.data;
     return sessionDetails;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const mockPutSessionDetails = async (sessionId: string, session: Session) => {
+  try {
+    const response: AxiosResponse<Session> = await mockApi.put(URL_SESSION_DETAILS + sessionId, session);
+    const sessionDetails: Session = response.data;
+    return sessionDetails;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/*      URL_ORDERS     */
+export const mockPostOrder = async (order: Order) => {
+  try {
+    const response: AxiosResponse<Order> = await mockApi.post(URL_ORDERS, order);
+    const mockOrder: Order = response.data;
+    return mockOrder;
   } catch (error) {
     console.error(error);
   }
