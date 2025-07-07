10ed8d0ed3395172cd436ac3d0d2bb92efdc9e7a
Soh <dexsjw@gmail.com>
2025-07-07 17:22:28 +0800

    deleted unused Props file

--- src/type-interface/props/SessionFormProps.ts
+++
@@ -1,5 +0,0 @@
-import { SessionUser } from "../SessionUser"
-
-export type SessionFormProps = {
-  // sessionUser: SessionUser
-}
--- src/type-interface/props/SessionUserFormProps.ts
+++
@@ -1,6 +0,0 @@
-import { SessionUser } from "../SessionUser"
-
-export type SessionUserFormProps = {
-  // sessionUser: SessionUser,
-  // handleSessionUserChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
-}
