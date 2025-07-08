e183acbd7aaf558a78619a39f87a3e6921614ecb
dextersjw <dextersjw@gmail.com>
2025-07-09 02:34:01 +0800

    updated isEditMode to editMode and exitEditMode()

--- src/type-interface/props/OrderFormProps.ts
+++ src/type-interface/props/OrderFormProps.ts
@@ -2,7 +2,8 @@ import { Order } from "../Order";
 
 export type OrderFormProps = {
   selectedBrandIndex: number,
-  isEditMode: boolean,
+  editMode: { isEdit: boolean, selectedOrderId: string },
+  exitEditMode: () => void,
   handleAddOrder: (order: Order) => void,
   handleUpdateOrder: (order: Order) => void,
   handleRemoveOrder: (orderId: string) => void
