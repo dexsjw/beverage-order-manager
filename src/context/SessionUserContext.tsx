66c9f35f4126c04d6005b603bb10510f528e1a9d
Soh <dexsjw@gmail.com>
2025-07-09 17:20:40 +0800

    added logic to monitor joinedSessions for 30 mins

--- src/context/SessionUserContext.tsx
+++ src/context/SessionUserContext.tsx
@@ -1,8 +1,10 @@
-import { createContext, ReactNode, useContext, useMemo, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useMemo, useState } from "react";
 import { SessionUserContextType } from "../type-interface/SessionUserContextType";
 import { SessionUser } from "../type-interface/SessionUser";
+import { JoinedSessions } from "../type-interface/JoinedSessions";
 
 const BOM_SESSION_USER_KEY = "bomSessionUserKey";
+const BOM_JOINED_SESSIONS_KEY = "bomJoinedSessionsKey";
 
 let currentSessionUser: SessionUser = { id: "", name: "" };
 const newSessionUser: SessionUser = { id: crypto.randomUUID(), name: "" };
@@ -19,6 +21,27 @@ try {
   localStorage.removeItem(BOM_SESSION_USER_KEY);
 }
 
+// 30 mins = 1,800,000 ms = 1.8e6 ms
+let currentJoinedSessions: JoinedSessions = {
+  sessionIds: [],
+  expiryTimestamp: new Date(Date.now() + 1.8e6).toLocaleString("en-GB")
+};
+const existingJoinedSessionsStr = localStorage.getItem(BOM_JOINED_SESSIONS_KEY);
+if (existingJoinedSessionsStr) {
+  try {
+    const existingJoinedSessions: JoinedSessions = JSON.parse(existingJoinedSessionsStr);
+    if (Date.parse(existingJoinedSessions.expiryTimestamp) > Date.now()) {
+      currentJoinedSessions = existingJoinedSessions;
+    } else {
+      console.warn("Removing key...");
+      localStorage.removeItem(BOM_JOINED_SESSIONS_KEY);
+    }
+  } catch (error) {
+    console.error(error);
+    localStorage.removeItem(BOM_JOINED_SESSIONS_KEY);
+  }
+}
+
 const SessionUserContext = createContext<SessionUserContextType | undefined>(undefined);
 
 export const useSessionUserContext = () => {
@@ -31,6 +54,23 @@ export const useSessionUserContext = () => {
 
 export function SessionUserProvider({ children }: Readonly<{ children: ReactNode }>) {
   const [sessionUser, setSessionUser] = useState<SessionUser>(currentSessionUser);
+  const [joinedSessions, setJoinedSessions] = useState<JoinedSessions>(currentJoinedSessions);
+
+  useEffect(() => {
+    const checkExpiryTimestampInterval = setInterval(() => {
+      if (Date.parse(joinedSessions.expiryTimestamp) < Date.now()) {
+        setJoinedSessions({
+          sessionIds: [],
+          expiryTimestamp: new Date(Date.now() + 1.8e6).toLocaleString("en-GB")
+        })
+        localStorage.removeItem(BOM_JOINED_SESSIONS_KEY);
+      }
+    }, 1.8e6);
+
+    return () => {
+      clearInterval(checkExpiryTimestampInterval);
+    };
+  }, []);
 
   const handleSessionUserChange = (sessionUserKey: string, sessionUserValue: string) => {
     const updatedSessionUser: SessionUser = {
@@ -41,12 +81,23 @@ export function SessionUserProvider({ children }: Readonly<{ children: ReactNode
     localStorage.setItem(BOM_SESSION_USER_KEY, JSON.stringify(updatedSessionUser));
   }
 
+  const handleAddSessionId = (sessionId: string) => {
+    const updatedJoinedSessions: JoinedSessions = {
+      ...joinedSessions,
+      sessionIds: [...joinedSessions.sessionIds, sessionId]
+    };
+    setJoinedSessions(updatedJoinedSessions);
+    localStorage.setItem(BOM_JOINED_SESSIONS_KEY, JSON.stringify(updatedJoinedSessions));
+  }
+
   const contextValue: SessionUserContextType = useMemo(() => {
     return {
       sessionUser,
-      handleSessionUserChange
+      joinedSessions,
+      handleSessionUserChange,
+      handleAddSessionId
     }
-  }, [sessionUser]);
+  }, [sessionUser, joinedSessions]);
 
   return (
     <SessionUserContext.Provider value={contextValue}>
