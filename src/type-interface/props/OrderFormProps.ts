5e74e215811ec16d37c554a3e0f80fead8fcb567
dextersjw <dextersjw@gmail.com>
2025-07-10 17:33:10 +0800

    removed exitEditMode
    removed

--- src/type-interface/props/OrderFormProps.ts
+++ src/type-interface/props/OrderFormProps.ts
@@ -3,7 +3,6 @@ import { Order } from "../Order";
 export type OrderFormProps = {
   selectedBrandIndex: number,
   orderToEdit: Order | null,
-  exitEditMode: () => void,
   handleAddOrder: (order: Order) => void,
   handleUpdateOrder: (order: Order) => void,
   handleRemoveOrder: (orderId: string) => void
